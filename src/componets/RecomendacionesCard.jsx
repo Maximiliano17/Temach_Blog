72b2e935f9474dbe852844386b92f8c2307b89ac
Maximiliano17 <maxidossantosburgos@gmail.com>
2024-06-06 14:09:16 -0600

    Subir cambios recientes

---
+++ src/componets/RecomendacionesCard.jsx
@@ -0,0 +1,13 @@
+import styles from "../modules/Comunidad.module.css"
+
+function RecoCards(){
+    return(
+    <>
+     <div className={styles.containerCard}>
+        
+     </div>
+    </>
+    )
+}
+
+export default RecoCards
--- src/views/CreadoresComunidad.jsx
+++ src/views/CreadoresComunidad.jsx
@@ -1,5 +1,6 @@
 import styles from "../modules/Comunidad.module.css"
 import Header from "../componets/Header"
+import RecoCards from "../componets/RecomendacionesCard"
 
 function Creadores(){
     return(
@@ -7,9 +8,13 @@ function Creadores(){
         <div className={styles.containerCreadores}>
          <Header />
           <section className={styles.creadoresHome}>
-           Pitos
+           <h1> Recomendados</h1>
+           <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nesciunt tenetur expedita fuga nemo placeat amet dolorum, quam impedit sunt, neque voluptatibus ullam. Totam commodi error magni debitis aspernatur ab!
+           </p>
           </section>
         </div>
+        <RecoCards />
         </>
     )
 }
